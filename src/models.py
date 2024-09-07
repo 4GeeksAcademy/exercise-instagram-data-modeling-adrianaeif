9f442cc17625c07bd6ddd3d3f65a3248d82d5116
Adriana Isea <adrianaisea02@gmail.com>
2024-09-07 03:19:30 +0000

    Terminado

--- src/models.py
+++ src/models.py
@@ -1,29 +1,81 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+import enum
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+# class Person(Base):
+#     __tablename__ = 'person'
+#     # Here we define columns for the table person
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+# class Address(Base):
+#     __tablename__ = 'address'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     street_name = Column(String(250))
+#     street_number = Column(String(250))
+#     post_code = Column(String(250), nullable=False)
+#     person_id = Column(Integer, ForeignKey('person.id'))
+#     person = relationship(Person)
+
+class MyEnum(enum.Enum):
+    video = 1
+    imagen = 2
+    historias = 3
+    post = 4
+
+class User(Base):
+    __tablename__ = 'User'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = name = Column(String(250))
+
+    def to_dict(self):
+        return {}
+
+class Post(Base):
+    __tablename__ = "Post"
+    id = Column(Integer, primary_key=True)
+    user_id =  Column(Integer, ForeignKey('User.id'))
+
+    def to_dict(self):
+        return {}
+
+class Media(Base):
+    __tablename__ = 'Media'
+    id = Column(Integer, primary_key=True)
+    type = Column(Enum(MyEnum), nullable=False)
+    url = Column(String(250), nullable=False)
+    post_id = Column(Integer, ForeignKey('User.id'))
+
+    def to_dict(self):
+        return {}
+
+class Comment(Base):
+    __tablename__ = "Comment"
+    id = Column(Integer, primary_key=True)
+    comment_text= Column(String(250), nullable=False)
+    author_id = Column(Integer, ForeignKey('User.username'))
+    post_id = Column(Integer, ForeignKey('Post.id'))
+
+    def to_dict(self):
+        return {}
+
+class Follower(Base):
+    __tablename__ = "Follower"
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('User.id'))
+    user_to_id = Column(Integer, ForeignKey('User.id'))
 
     def to_dict(self):
         return {}
